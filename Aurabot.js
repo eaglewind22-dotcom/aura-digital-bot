a0ad2ddf4415a37687c9bd29794b34410ef93210
eaglewind22-dotcom <eaglewind22@gmail.com>
2026-06-29 16:12:28 +0630

    Update Aurabot.js

--- Aurabot.js
+++ Aurabot.js
@@ -1,8 +1,10 @@
 const { Telegraf } = require('telegraf');
+
 const BOT_TOKEN = '8953970980:AAG-8uV8P9ni_vlmXVGdTZfdVrSFHuosX3Y';
 const bot = new Telegraf(BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('✅ Bot အလုပ်လုပ်နေပါပြီ!'));
+bot.start((ctx) => ctx.reply('Bot အလုပ်လုပ်နေပါပြီ!'));
 
-// Webhook အပိုင်းတွေ အကုန်ဖြုတ်ပြီး Polling နဲ့ Launch လုပ်မယ်
-bot.launch().then(() => console.log('🚀 Bot launched via Polling!'));
+bot.launch().then(() => {
+    console.log('Bot ပွင့်နေပါပြီ။');
+});
